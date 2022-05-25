22d4db05486e522906b27bbbc61b193096227918
krish <krish@MSI>
2022-05-25 17:26:49 +0530

    First Repository

---
+++ src/inheritance.java
@@ -0,0 +1,39 @@
+
+
+
+	
+
+	
+	class Name{
+		
+		 void Library() {
+			 
+			 System.out.println("BIBLE");
+		 }
+		
+		}
+		
+	class book extends Name {
+		
+		
+		void gfdt () {
+		
+		System.out.println("bible");
+		
+		}
+		
+	}
+	public class inheritance {
+		
+	public static void main(String [] args)	{
+		Name o = new book();
+		o.Library();
+		  
+		 
+		
+		
+	}	
+	
+	
+	
+}
